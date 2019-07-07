c98318641fec67cd225a35db04cd7853934dce45
Abdurrahman Akın Aktaş <akin.a.aktas@gmail.com>
2019-07-08 00:09:32 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,27 +5,31 @@ from agent import DQNAgent
 
 LOAD_MODEL = "models/2x256__-17350.00max_-22770.00avg_-27850.00min__1562545118.model"
 
+ROUNDS = 5
+
 env = environment()
 
 if __name__ == '__main__':
     agent = DQNAgent()
 
-for round in range(5):
-    # start the environment
-    current_state = env.reset()
+    for round in range(ROUNDS):
+        # start the environment
+        current_state = env.reset()
 
-    done = False
+        done = False
 
-    while not done:
+        while not done:
 
-        last_time = time.time()
-       
-        action = np.argmax(agent.get_qs(current_state))
-
-        new_state, reward, done = env.step(action)
-
-        current_state = new_state
+            last_time = time.time()
         
-        print(f"Action: {action} | Loop took: {time.time() - last_time}")
+            # determine action with highest Q-value
+            action = np.argmax(agent.get_qs(current_state))
+            # apply action and receive new state
+            new_state, reward, done = env.step(action)
+            # update state
+            current_state = new_state
+            
+            print(f"Action: {action} | Loop took: {time.time() - last_time}")
 
-    print(f'Final Score for round{round}: {env.score}')
+        print(f'Final Score for round{round}: {env.score}')
+        time.sleep(1)
